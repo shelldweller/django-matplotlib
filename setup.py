66d1c4babdd0d2000c721175dd2b4896a473de1d
Sergiy Kuzmenko <sergiyk@lexmark.com>
2013-07-11 09:40:52 -0400

    added setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+# Dynamically calculate the version based on mptt.VERSION
+
+setup(
+    name = 'django-matplotlib',
+    description = '''Django interface to matplotlib''',
+    version = '0.1-pre-alpha',
+    author = 'Sergiy Kuzmenko',
+    author_email = 'sergiy@kuzmenko.org',
+    url = 'https://github.com/shelldweller/django-matplotlib',
+    packages=['django_matplotlib'],
+    classifiers = ['Development Status :: 2 - Pre-Alpha',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: Public Domain',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   ],
+)
